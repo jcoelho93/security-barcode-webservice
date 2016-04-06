dcbbeda5a32281aece26ebea2e318ae53cbb6f9a
jcamilo93 <camilocoelho93@gmail.com>
2016-04-06 23:01:30 +0100

    Encryptor class

---
+++ jaxrs-server/src/main/java/encryption/AESEncryptor.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package encryption;
+
+/**
+ *
+ * @author Camilo
+ */
+public class AESEncryptor implements EncryptionInterface{
+
+    public byte[] encrypt(String data)
+    {
+        
+    }
+    
+    public String decrypt(byte[] data)
+    {
+        return new String();
+    }
+    
+    public void generateKeys()
+    {
+        
+    }
+    
+}
--- jaxrs-server/src/main/java/encryption/Encryptor.java
+++ jaxrs-server/src/main/java/encryption/Encryptor.java
@@ -11,4 +11,54 @@ package encryption;
  */
 public class Encryptor {
     
+    
+    private EncryptionInterface encryptor;
+    
+    private String data;
+    private byte[] encrypted;
+    
+    public Encryptor(EncryptionInterface encryptor)
+    {
+        
+        this.encryptor = encryptor;
+        
+    }
+    
+    public void setEncryptorClass(EncryptionInterface encryptor)
+    {
+        
+        this.encryptor = encryptor;
+        
+    }
+    
+    public EncryptionInterface getEncryptor()
+    {
+        
+        return this.encryptor;
+        
+    }
+    
+    public byte[] encrypt(String data)
+    {
+        this.data = data;
+        this.encrypted = this.encryptor.encrypt(data);
+        return this.encrypted;
+    }
+    
+    public String decrypt(byte[] data)
+    {
+        
+        this.encrypted = data;
+        this.data = this.encryptor.decrypt(data);
+        return this.data;
+        
+    }
+    
+    public String hash(String data)
+    {
+        
+        return data;
+        
+    }
+    
 }
